33882a8bba25be1e77768330deda2d0caf6c737c
Samarth175 <samarth1751997@gmail.com>
2020-03-01 18:12:31 +0530

    ck dataset added

--- playground.py
+++ playground.py
@@ -1,5 +1,5 @@
 import cv2
-img = cv2.imread('./datasets/training-set/angry/KM.AN3.19.jpeg')		# assuming color image	
+img = cv2.imread('./datasets/training-set/angry/S037_003_00000020.png')		# assuming color image	
 print(img.shape) 	
 
 
--- trainmodel.py
+++ trainmodel.py
@@ -22,7 +22,8 @@ from keras.layers import Dense
 classifier = Sequential()
 
 # Step 1 - Convolution
-classifier.add(Convolution2D(32, (3, 3), input_shape = (64, 64, 3), activation = 'relu'))
+#classifier.add(Convolution2D(32, (3, 3), input_shape = (64, 64, 3), activation = 'relu'))
+classifier.add(Convolution2D(32, (3, 3), input_shape = (48, 48, 3), activation = 'relu'))
 
 # Step 2 - Pooling
 classifier.add(MaxPooling2D(pool_size = (2, 2)))
@@ -53,19 +54,28 @@ train_datagen = ImageDataGenerator(rescale = 1./255,
 test_datagen = ImageDataGenerator(rescale = 1./255)
 
 training_set = train_datagen.flow_from_directory('datasets/training-set',
-                                                 target_size = (64, 64),
+#                                                 target_size = (64, 64),
+                                                 target_size = (48, 48),
                                                  batch_size = 32,
                                                  class_mode = 'categorical')
 
 test_set = test_datagen.flow_from_directory('datasets/test-set',
-                                            target_size = (64, 64),
+#                                            target_size = (64, 64),
+                                            target_size = (48, 48),
                                             batch_size = 32,
                                             class_mode = 'categorical')
 
+#classifier.fit_generator(training_set,
+#                         steps_per_epoch = 170,                                               
+#                         epochs = 25,
+#                         validation_data = test_set,
+#                         validation_steps = 42)
+
 classifier.fit_generator(training_set,
-                         steps_per_epoch = 170,                                               
+#                         steps_per_epoch = 744,
+                         steps_per_epoch = 24,    #744/32 = 23.25                                               
                          epochs = 25,
                          validation_data = test_set,
-                         validation_steps = 42)
+                         validation_steps = 183)
 
 classifier.save('my_model.h5',overwrite=True)
